08d87ae2e719f582409d8640c56c277471880b7e
syairaarif <putrisyafira4444@gmail.com>
2024-05-11 11:01:39 +0700

    membuat case bagian kedua

--- stackusinglinkedlist_0194/stackusinglinkedlist_0194.cpp
+++ stackusinglinkedlist_0194/stackusinglinkedlist_0194.cpp
@@ -94,6 +94,12 @@ int main()
             cin >> value;
             stack.push(value);
             break;
+        case 2:
+            if (!stack.isEmpty()) {
+                stack.pop();
+            }
+            else {
+                cout << "stack is empty. Cannot pop." << endl;
         }
 
 
