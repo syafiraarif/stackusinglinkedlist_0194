fe8c2f944b4618d78784204f0dacab87ff9198ce
syairaarif <putrisyafira4444@gmail.com>
2024-05-11 10:54:29 +0700

    memperbaki kesalahan peletakan

--- stackusinglinkedlist_0194/stackusinglinkedlist_0194.cpp
+++ stackusinglinkedlist_0194/stackusinglinkedlist_0194.cpp
@@ -24,50 +24,53 @@ public:
     {
         top = NULL;
     }
+
+    int push(int value)
+    {
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "push value: " << value << endl;
+        return value;
+    }
+
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty." << endl;
+        }
+        cout << "Poped value: " << top->data << endl;
+        top = top->next;
+    }
+
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is Empty." << endl;
+        }
+        else
+        {
+            Node* current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
+
+    bool isEmpty()
+    {
+        return top == NULL;
+    }
+
 };
 
-int push(int value) 
-{
-    Node* newNode = new Node();
-    newNode->data = value;
-    newNode->next = top;
-    top = newNode;
-    cout << "push value: " << value << endl;
-    return value;
-}
 
-void pop() 
-{
-    if (isEmpty()) 
-    {
-        cout << "Stack is empty." << endl;
-    }
-    cout << "Poped value: " << top->data << endl;
-    top = top->next;
-}
-
-void peek() 
-{
-    if (top == NULL) 
-    {
-        cout << "List is Empty." << endl;
-    }
-    else 
-    {
-        Node* current = top;
-        while (current != NULL) 
-        {
-            cout << current->data << " " << endl;
-            current = current->next;
-        }
-        cout << endl;
-    }
-}
-
-bool isEmpty() 
-{
-    return top == NULL;
-}
 int main()
 {
     
